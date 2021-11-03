2ab73f9a0d1a797c2228e2ffa7653b78c8053683
Julius Kramer <juliuskramer00@gmail.com>
2021-11-03 19:38:32 +0100

    added welcome screen while firefox is loading

--- scripts/start_display.sh
+++ scripts/start_display.sh
@@ -3,5 +3,7 @@ if [ "$EUID" -ne 0 ]; then
 	echo "Please run this script as root"; exit 1;
 fi
 
+DISPLAY=:0 pcmanfm --set-wallpaper /home/pi/Raspberry_Pi_Kiosk_Display/images/desktop_welcome.jpg &
+DISPLAY=:0 pcmanfm --desktop-off &
 unclutter --display=:0 &
 firefox --display=:0 --kiosk $(head -n 1 /home/pi/Raspberry_Pi_Kiosk_Display/website.txt) &
